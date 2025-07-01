b3ea558e780ab45fb93a6c59c521fc0b498ae521
srikeerthireddy <srikeerthireddy24@gmail.com>
2025-07-01 17:00:05 +0530

    Frontend changes

--- frontend/cricket-frontend/src/socket.js
+++ frontend/cricket-frontend/src/socket.js
@@ -1,4 +1,4 @@
 // socket.js
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:3000');
+const socket = io('https://real-time-cricket-team-selection-room-bvmw.onrender.com/');
 export default socket;
